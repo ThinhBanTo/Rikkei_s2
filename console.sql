5b52cd4208790a32b95f578464f8c5ae3ceef99a
Nguyễn Khắc Thịnh <thinhnguacon@gmail.com>
2026-03-19 18:22:12 +0700

    Delete console.sql

--- console.sql
+++
@@ -1,8 +0,0 @@
--- --comment 1 dòng
--- --1. tạo 1 cơ sở dữ liệu có tên là JV260316_DB
--- --syntax: create database [database_name]
--- --trong postgres không phân biệt hoa thường
--- create database JV260316_DB
--- --2. Xóa cơ sở db
--- --syntax: Drop Database [datebase_name]
-create database EcommerceDB
